fec1f354527c531d898b74bf13b867e77e2db626
Andy Grunwald <andygrunwald@gmail.com>
2015-09-21 20:45:15 +0200

    Added basic version for subcommands

--- main.go
+++ main.go
@@ -1,26 +1,55 @@
 package main
 
 import (
-	"flag"
 	"fmt"
+	"github.com/codegangsta/cli"
 	"os"
 )
 
 const (
-	Name    = "Watson"
+	// Name is the name of Watson :)
+	Name = "Watson"
+	// Version is the current version of Watson
 	Version = "0.0.1"
+	// Usage is a small and catchy sentence
+	Usage = "Crawl your Gerrit!"
+	// Author is the name of the app author
+	Author = "Andy Grunwald"
+	// Email is the email of the app author
+	Email = "andygrunwald@gmail.com"
 )
 
 func main() {
-	var (
-		version = flag.Bool("version", false, "Prints the version and exits")
-	)
-	flag.Parse()
-
-	if *version {
-		fmt.Printf("%s v%s\n", Name, Version)
-		os.Exit(0)
+	app := cli.NewApp()
+	app.Name = Name
+	app.Version = Version
+	app.Author = Author
+	app.Email = Email
+	app.Usage = Usage
+	app.Action = func(c *cli.Context) {
+		fmt.Printf("Hi, i am %s. Nice to meet you.\n", Name)
+		fmt.Println("Use the -help parameter to get more information how to use me!")
+	}
+	/*
+		app.Flags = []cli.Flag{
+			cli.StringFlag{
+				Name:   "instance, i",
+				Value:  "",
+				Usage:  "URL for the Gerrit instance",
+				EnvVar: "WATSON_INSTANCE",
+			},
+		}
+	*/
+	app.Commands = []cli.Command{
+		{
+			Name:    "list-projects",
+			Aliases: []string{"lp"},
+			Usage:   "Lists all projects of a Gerrit instance",
+			Action: func(c *cli.Context) {
+				println("added task: ", c.Args().First())
+			},
+		},
 	}
 
-	fmt.Println("Hi, i am Watson. Nice to meet you.")
+	app.Run(os.Args)
 }
