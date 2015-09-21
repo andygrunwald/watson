fb73cb0a86056428358a522e1afab6e48bffd40b
Andy Grunwald <andygrunwald@gmail.com>
2015-09-21 20:26:29 +0200

    Added version printing

--- main.go
+++ main.go
@@ -1,9 +1,26 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
+)
+
+const (
+	Name    = "Watson"
+	Version = "0.0.1"
 )
 
 func main() {
+	var (
+		version = flag.Bool("version", false, "Prints the version and exits")
+	)
+	flag.Parse()
+
+	if *version {
+		fmt.Printf("%s v%s\n", Name, Version)
+		os.Exit(0)
+	}
+
 	fmt.Println("Hi, i am Watson. Nice to meet you.")
 }
