c339335e985642f4c2e9031139d82f52c776dec6
Andy Grunwald <andygrunwald@gmail.com>
2015-10-19 21:14:03 +0200

    Add new type (ApprovalInfo) and avoid nil structs if identity is empty

--- storage/identity/identify.go
+++ storage/identity/identify.go
@@ -14,7 +14,13 @@ type AccountInfo gerrit.AccountInfo
 
 type EmailInfo gerrit.EmailInfo
 
+type ApprovalInfo gerrit.ApprovalInfo
+
 func (p *GitPersonInfo) Identify() *Identity {
+	if len(p.Name) == 0 && len(p.Email) == 0 {
+		return nil
+	}
+
 	i := &Identity{
 		Name:  p.Name,
 		Email: p.Email,
@@ -24,6 +30,10 @@ func (p *GitPersonInfo) Identify() *Identity {
 }
 
 func (p AccountInfo) Identify() *Identity {
+	if len(p.Name) == 0 && len(p.Email) == 0 && len(p.Username) == 0 {
+		return nil
+	}
+
 	i := &Identity{
 		Name:     p.Name,
 		Email:    p.Email,
@@ -32,7 +42,26 @@ func (p AccountInfo) Identify() *Identity {
 
 	return i
 }
+
+func (p ApprovalInfo) Identify() *Identity {
+	if len(p.Name) == 0 && len(p.Email) == 0 && len(p.Username) == 0 {
+		return nil
+	}
+
+	i := &Identity{
+		Name:     p.Name,
+		Email:    p.Email,
+		Username: p.Username,
+	}
+
+	return i
+}
+
 func (p *EmailInfo) Identify() *Identity {
+	if len(p.Email) == 0 {
+		return nil
+	}
+
 	i := &Identity{
 		Email: p.Email,
 	}
